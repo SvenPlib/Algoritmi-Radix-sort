78da2730492fecdf142d0058134d53181ed8323f
SvenPlib <sven.plibersek@student.um.si>
2025-03-20 21:29:11 +0100

    Added functions for read and write to file

--- DomacaNaloga1.cpp
+++ DomacaNaloga1.cpp
@@ -5,6 +5,31 @@
 
 using namespace std;
 
+bool Branje_Stevil(vector<unsigned char>& vec, const char s[]) {
+    ifstream input(s);
+    int st;
+
+    if (!input.is_open()) {
+        return false;
+    }
+
+    while (!input.eof()) {
+        input >> st;
+        vec.push_back(st);
+        while (isspace(input.peek())) input.get();
+    }
+    input.close();
+    return true;
+}
+
+void Izpis_Stevil(vector<unsigned char>& polje) {
+    ofstream output("out.txt");
+
+    for (int i = 0; i < polje.size(); i++)
+        output << static_cast<int>(polje[i]) << ' ';
+}
+
+
 int main()
 {
     cout << "Hello World!"<<endl;
